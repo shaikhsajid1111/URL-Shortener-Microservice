4580f8f9274e83ccc9de64e73ae3e682b24c4dfe
Glitch (bittersweet-aware-saguaro) <none>
2020-12-08 10:11:38 +0000

    updated with glitch

---
+++ routes/index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require("mongoose");
+const validUrl = require("valid-url");
+const shortId = require("shortid");
+
+const URL = require("../models/urlSchema");
+
+
+
+const URI = process.env.DB_URI;
+/*connecting the database*/
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+
+const connection = mongoose.connection;
+connection.on("error",() =>{
+  console.log("Connection error")
+})
+
+connection.on("open",() =>{
+  console.log("Connection established!")
+})
+
+
+router.get("/:short_url?",async (req,res,next) =>{
+  try{
+      
+      const url_params = await URL.findOne({short_url : req.params.short_url});
+    
+    if(url_params){
+      return res.redirect(url_params.original_url)
+    }
+    else{
+      return res.json("No URL found")
+    }
+    
+  }
+  catch(err){
+    console.log(err)
+    res.json({
+      error : "Server Error"
+    })
+  }
+})
+
+
+
+
+router.post("/new",async (req,res,next) =>{
+  console.log(req.body)
+  console.log("reached")
+  const url = req.body.url
+  const urlCode = shortId.generate()
+  
+  if (!validUrl.isWebUri(url)) {
+    res.json({
+      error: 'Invalid URL'
+    })
+  } else {
+    try {
+      // check if its already in the database
+      let findOne = await URL.findOne({
+        original_url: url
+      })
+      if (findOne) {
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        })
+      } else {
+        
+        let newOne = new URL({
+          original_url: url,
+          short_url: urlCode
+        })
+        await newOne.save()
+        res.json({
+          original_url: newOne.original_url,
+          short_url: newOne.short_url
+        })
+      }
+    } catch (err) {
+      console.error(err)
+      res.json('Server erorr...')
+    }
+  }  
+})
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+
+const route = require("./routes/index");
+const cors = require("cors");
+app.use(bodyParser.json());
+app.use(cors());
+
+/*middleware for GET request*/
+app.use("/",route)
+
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
